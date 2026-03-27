3bbfc087dc80df61305c04621dc1ffa3b4daf610
Yujin <yujin-98@naver.com>
2026-03-27 14:15:07 +0900

    update: src html message fixed

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "<h1>성공! a3028 앱이 정상적으로 동작 중입니다.</h1>";
+        return "<h1>성공! a3028 앱이 정상적으로 동작 중입니다~~</h1>";
     }
 }
